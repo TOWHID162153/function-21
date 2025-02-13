f0b1096b9c379db1588d3df57f9d8af185a615e3
TOWHID <web.development162253@gmail.com>
2025-02-13 23:56:25 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,17 @@
+// function
+console.log("FUCNTION hoolo boro vai");
+console.log("khali order korbe ar");
+console.log("tar choto vai ra tar jonno kaj kore dibe");
+// /---------------------/ 
+console.log("function ekhon tar choto vai k fan on korar order dilo")
+function fname(){
+    console.log("bosha theke uthe dara")
+    console.log("switch er kache ja")
+    console.log("switch a cap de")
+    console.log("tar pore vag eikhan theke")
+}
+fname();
+
+// function mathSolution(num){
+//     const sum = 
+// }
